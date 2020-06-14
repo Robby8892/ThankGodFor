bd779fa0eeddd59e2114eea83b03c2cdbfa53f71
roberto cortes <robertcortesgrimm223@gmail.com>
2020-06-14 17:49:12 -0500

    admin requirement for created,update,delete worrks

--- routes/treat-router.js
+++ routes/treat-router.js
@@ -4,16 +4,17 @@ const multer = require('multer')
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage})
 
+// this will be added to each routet that only the admin can change 
+const verifyIfAdmin = require('../lib/verifyIfAdmin.js')
 
 const router = express.Router()
 
 router.get('/treat/:id', treatController.getOneTreat)
 router.get('/treat', treatController.getAllTreats)
 
-const verifyIfAdmin = require('../lib/verifyIfAdmin.js')
 
-router.post('/treat/new', upload.single('imgOfTreat'), treatController.createTreat)
-router.delete('/treat/:id', treatController.deleteTreat)
-router.put('/treat/:id/edit', treatController.updateTreat)
+router.post('/treat/new', verifyIfAdmin, upload.single('imgOfTreat'), treatController.createTreat)
+router.delete('/treat/:id', verifyIfAdmin, treatController.deleteTreat)
+router.put('/treat/:id/edit', verifyIfAdmin, treatController.updateTreat)
 
 module.exports = router
