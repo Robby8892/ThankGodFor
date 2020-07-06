6eb63d40cc7a95b71600fa679bbfc89fa5c4fac6
roberto cortes <robertcortesgrimm223@gmail.com>
2020-07-05 19:15:01 -0500

    login route for user create

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,7 @@
 const session = require('express-session')
 const bcrypt = require('bcrypt')
 const shortId = require('shortid')
+const User = require('../models/user.js')
 
 
 console.log('here is user_________', User);
@@ -29,6 +30,54 @@ createUser = async (req,res,error) => {
 			})
 		})
 
+	}catch(error){
+		console.log(error);
+	}
+
+}
+
+
+
+loginUser= async (req,res, error) => {
+	try {
+	
+		const body = req.body.data || req.body
+
+		const findUser = await User.findOne({username: body.username})
+
+		if(!findUser){
+			console.log('Can\'t find user.');
+
+			return res.json({
+				status: 400,
+				success: false,
+				error: 'Login name was invalid'
+			})
+		}
+
+		const passwordValidation = bcrypt.compareSync(body.password, findUser.password)
+
+		if(passwordValidation){
+			console.log('passward is valid!');
+
+
+			return res.status(200).json({
+				status: 200,
+				email: req.body.email,
+				id: findUser._id,
+				success: true
+			})
+
+
+
+		} else {
+			console.log('password is invalid.');
+			return res.json({
+				status: 400,
+				success: false,
+				error: 'Invalid password.'
+			})
+		}
 	}catch(error){
 		console.log(error);
 	}
